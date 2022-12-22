2d0b6b839d521e9662d772c12c1a2663700b81cb
Tobias Schmidt <cpl_tobi@gmx.de>
2022-12-22 15:17:39 +0100

    fix css
    buttons and styling

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </section>
 
     <section id="about" class="space-top">
-        <h2>Lorem, ipsum dolor.</h2>
+        <h2>TestLorem, ipsum dolor.</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed perspiciatis illum cupiditate repudiandae!</p>
     </section>
 
